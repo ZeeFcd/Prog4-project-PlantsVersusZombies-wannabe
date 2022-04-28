e574c937f0c9ddfd0829be08e2d3cbac32eac99f
NagyPatrik024 <nagy.patrik024@gmail.com>
2022-04-28 13:51:10 +0200

    GameAnimationBurshes

--- GUI_20212202_IJA9WQ/Assets/GameAnimationBrushes.cs
+++ GUI_20212202_IJA9WQ/Assets/GameAnimationBrushes.cs
@@ -9,9 +9,31 @@ using System.Windows.Media.Imaging;
 
 namespace GUI_20212202_IJA9WQ.Assets
 {
-    public static class GameAnimationBrushes
+    public class GameAnimationBrushes
     {
-        public static readonly Brush SunflowerNormal1 = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images","Animations" ,"background_day.png"), UriKind.RelativeOrAbsolute)));
+        public List<Brush> PeashooterGIFS;
+        public List<Brush> SunfloowerGIFS;
+        public List<Brush> SnowpeashooterGIFS;
+        public List<Brush> PotatomineGIFS;
+        public List<Brush> WallnutGIFS;
 
+        public GameAnimationBrushes()
+        {
+            PeashooterGIFS = BrushesReader("peashooterAnimation");
+            SunfloowerGIFS = BrushesReader("sunflowerAnimation");
+            SnowpeashooterGIFS = BrushesReader("snowpeashooterAnimation");
+            PotatomineGIFS = BrushesReader("potatomineAnimation");
+            WallnutGIFS = BrushesReader("wallnutAnimation");
+        }
+        public List<Brush> BrushesReader(string path)
+        {
+            List<Brush> brushes = new List<Brush>();
+            string[] files = Directory.GetFiles(Path.Combine("Images", path));
+            foreach (string file in files)
+            {
+                brushes.Add(new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", Path.GetFileName(file)), UriKind.RelativeOrAbsolute))));
+            }
+            return brushes;
+        }
     }
 }
