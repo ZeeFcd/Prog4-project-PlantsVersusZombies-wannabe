232182239e0adb588d3f751d5b4dcdc204973698
ZeeF <ZeeF@stud.uni-obuda.hu>
2022-05-07 01:46:53 +0200

    delete not used brushes

--- GUI_20212202_IJA9WQ/Assets/GameBrushes.cs
+++ GUI_20212202_IJA9WQ/Assets/GameBrushes.cs
@@ -14,9 +14,6 @@ namespace GUI_20212202_IJA9WQ.Assets
         public static readonly Brush BackgroundDayBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "background_day.png"), UriKind.RelativeOrAbsolute)));     
         public static readonly Brush LawnMoverBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "lawn_mover.png"), UriKind.RelativeOrAbsolute)));
         public static readonly Brush ItemShopBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "itemShop.png"), UriKind.RelativeOrAbsolute)));
-        public static readonly Brush PeashooterBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "peashooter.png"), UriKind.RelativeOrAbsolute)));
-        public static readonly Brush SunflowerBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "sunflower.png"), UriKind.RelativeOrAbsolute)));
-        public static readonly Brush ZombieBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "thiller.gif"), UriKind.RelativeOrAbsolute)));
         public static readonly Brush SunBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "sun1.png"), UriKind.RelativeOrAbsolute)));
         public static readonly Brush PeaBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "pea.png"), UriKind.RelativeOrAbsolute)));
         public static readonly Brush SnowPeaBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "snowpea.png"), UriKind.RelativeOrAbsolute)));
