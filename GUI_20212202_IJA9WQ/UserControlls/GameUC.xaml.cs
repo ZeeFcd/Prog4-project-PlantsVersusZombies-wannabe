3c60e749b32e7444c7c7b7c35e3d369193357b60
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2022-05-07 14:34:41 +0200

    UCs added, vms added

---
+++ GUI_20212202_IJA9WQ/UserControlls/GameUC.xaml.cs
@@ -0,0 +1,33 @@
+﻿using GUI_20212202_IJA9WQ.Logic;
+using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace GUI_20212202_IJA9WQ.UserControlls
+{
+    /// <summary>
+    /// Interaction logic for GameUC.xaml
+    /// </summary>
+    public partial class GameUC : UserControl
+    {
+
+        GameLogic logic;
+        DispatcherTimer dt = new DispatcherTimer();
+
+        public GameUC()
+        {
+            logic = new GameLogic((int)grid.ActualWidth, (int)grid.ActualHeight);
+            InitializeComponent();
+            display.SetupLogic(logic);
+            display.InvalidateVisual();
+            dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(17);
+            dt.Tick += (sender, eventargs) =>
+            {
+                logic.TimeStep();
+                display.InvalidateVisual();
+            };
+            dt.Start();
+        }
+    }
+}
---
+++ GUI_20212202_IJA9WQ/ViewModels/MenuViewModel.cs
@@ -0,0 +1,37 @@
+﻿using GUI_20212202_IJA9WQ.Logic;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GUI_20212202_IJA9WQ.ViewModels
+{
+    public class MenuViewModel : ObservableRecipient
+    {
+        public ICommand StartGameCommand { get; set; }
+        public ICommand MuteCommand { get; set; }
+        public IViewLogic viewLogic { get; set; }
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                return
+                    (bool)DependencyPropertyDescriptor
+                    .FromProperty(DesignerProperties.
+                    IsInDesignModeProperty,
+                    typeof(FrameworkElement))
+                    .Metadata.DefaultValue;
+            }
+        }
+        public MenuViewModel() : this(IsInDesignMode ? null : Ioc.Default.GetService<IViewLogic>()) { }
+        public MenuViewModel(IViewLogic viewLogic)
+        {
+            this.viewLogic = viewLogic;
+            StartGameCommand = new RelayCommand(() => viewLogic.ChangeView("game"));
+        }
+
+
+    }
+}
