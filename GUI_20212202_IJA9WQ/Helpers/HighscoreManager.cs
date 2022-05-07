5c498a30f9a249bc20c5ea5ef91520847dcdd6a9
ZeeF <ZeeF@stud.uni-obuda.hu>
2022-05-07 23:22:01 +0200

    highscore class created

---
+++ GUI_20212202_IJA9WQ/Helpers/HighscoreManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212202_IJA9WQ.Helpers
+{
+    class HighscoreManager
+    {
+    }
+}
