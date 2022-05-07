b1cd63a07f74c0c33579be1c9a8ced5f9fd30ca1
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2022-05-07 14:31:53 +0200

    viewLogic added

---
+++ GUI_20212202_IJA9WQ/Logic/ViewLogic.cs
@@ -0,0 +1,31 @@
+﻿using GUI_20212202_IJA9WQ.UserControlls;
+using Microsoft.Toolkit.Mvvm.Messaging;
+
+namespace GUI_20212202_IJA9WQ.Logic
+{
+    public class ViewLogic : IViewLogic
+    {
+        public object View { get; set; }
+        IMessenger messenger;
+        public ViewLogic(IMessenger messenger)
+        {
+            
+            this.messenger = messenger;
+        }
+        public void ChangeView(string view)
+        {
+            switch (view)
+            {
+                case "game":
+                    View = new GameUC();
+                    break;
+                case "menu":
+                    View = new MenuUC();
+                    break;
+                default:
+                    break;
+            }
+            messenger.Send("viewChanged", "Base");
+        }
+    }
+}
